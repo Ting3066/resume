500eee911246fc10df012ef120419a1b33381130
WenTing <mybaby0103066@gmail.com>
2021-01-19 16:16:07 +0800

    點擊選單時會以滑動的方式前往目標區域，增加skill區塊

--- js.js
+++ js.js
@@ -1,11 +1,41 @@
 $(document).ready(function(){
 
+  $("#menu a").click(function(){
+    const theid=$(this).attr("href");
+    const val=$(theid).offset().top-$("#myMenu").innerHeight()+1;
+
+    $("html").animate(
+      {scrollTop:val},1000,"swing"
+    );
+    
+  });
+  
+
+
+  function spy(){  //檢查目前滾動位置與對應MENU之ACTIVE
+    const nowat=$(document).scrollTop();
+    $("section").each(function(){
+      // console.log(this)
+      
+      const 
+      offset=$(this).offset().top,
+      height=$(this).innerHeight(),
+      id=$(this).attr('id')
+      if(offset<=nowat && nowat<offset+height){ //目前高度落於該section內
+        $('#myMenu a').not(`a[href='#${id}']`).removeClass('active');
+        $(`#myMenu a[href='#${id}']`).addClass('active');
+      }
+    })
+  }
+  
+  
+
   function bgmenu(){
     const 
       nowat=$(document).scrollTop(),
       height=$("#about").innerHeight(),
       offset=$("#myMenu").innerHeight(),
-      screenW=$(document).innerWidth(),
+      screenW=$(document).innerWidth();
 
 
     if(screenW>=992){ 
@@ -24,8 +54,11 @@ $(document).ready(function(){
   }
 
   $(document).scroll(function(){  
+    spy();
+    bgmenu();
   });
   $(window).resize(bgmenu); 
+  spy();
   bgmenu();
 
 })
